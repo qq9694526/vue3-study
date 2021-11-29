b27685843ef331ec4493a51476d9b116f36718bf
赵海鹏 <9694526@qq.com>
2021-11-29 15:49:44 +0800

    feat:composition-hook

---
+++ src/pages/03.composition-hook.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    大家好，我是{{ defaultName }}。
+    <br />
+    我的特长有
+    <p v-for="(item, index) in myTags" :key="index">{{ item }}</p>
+  </div>
+</template>
+<script>
+import { onMounted, onUpdated, ref } from 'vue'
+// setup中的生命周期函数
+
+export default {
+  setup(props, context) {
+    let defaultName = ref("码农胖大海");
+    // 和Vue的生命周期是一致的，除了create，因为setup本身就是这个阶段。
+    onMounted(() => {
+      console.log('mounted!')
+      // 更新数据以触发update
+      defaultName.value = "mounted后的大海"
+    })
+    onUpdated(() => {
+      console.log('updated!')
+    })
+    // 更多钩子参看官网 https://v3.cn.vuejs.org/guide/composition-api-lifecycle-hooks.html
+    return {
+      defaultName,
+    };
+  },
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import {createRouter,createWebHashHistory} from "vue-router";
 import HelloWorld from './pages/HelloWorld.vue'
 import Setup from './pages/01.composition-setup.vue'
 import Ref from './pages/02.composition-ref.vue'
+import Hook from './pages/03.composition-hook.vue'
 
 // 2. 定义一些路由
 // 每个路由都需要映射到一个组件。
@@ -13,6 +14,7 @@ const routes = [
   { path: '/hello', component: HelloWorld },
   { path: '/setup', component: Setup },
   { path: '/ref', component: Ref },
+  { path: '/hook', component: Hook },
 ]
 
 // 3. 创建路由实例并传递 `routes` 配置
